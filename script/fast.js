e8911338b6f6540a6e318326e35f01426ea4d98a
akferdous56 <ferdousvai81@gmail.com>
2025-03-02 23:02:03 +0600

    first commit

---
+++ script/fast.js
@@ -0,0 +1,161 @@
+   document.getElementById("btn-1").addEventListener("click",function(){
+    
+    alert("bord update sucessfully")
+ const count = document.getElementById("count-num").innerText
+ const sub = count - 1
+ document.getElementById("count-num").innerText = sub
+
+
+ const add = document.getElementById("count-add").innerText
+ const convertNum = parseInt(add)
+ const sum = convertNum +1
+ document.getElementById("count-add").innerText = sum
+  
+ const container = document.getElementById("trans-data")
+ 
+ const paid = document.createElement("p")
+ paid.innerText=` You have Complete The Fix Mobile Button Issue at 12:48:15 PM
+ `
+ 
+container.appendChild(paid)
+
+document.getElementById("btn-1").disabled = true
+
+})
+
+// button 2 
+
+
+document.getElementById("btn-2").addEventListener("click" , function(){
+    alert("bord update sucessfully")
+    const count = document.getElementById("count-num").innerText
+    const sub = count - 1
+    document.getElementById("count-num").innerText = sub
+   
+   
+    const add = document.getElementById("count-add").innerText
+    const convertNum = parseInt(add)
+    const sum = convertNum +1
+    document.getElementById("count-add").innerText = sum
+    
+    const container = document.getElementById("trans-data")
+ 
+ const paid = document.createElement("p")
+ paid.innerText=` You have Complete The Task Add Dark Mode at 12:48:15 PM
+ `
+ 
+container.appendChild(paid)
+
+    document.getElementById("btn-2").disabled = true
+})
+
+
+// button----3
+
+document.getElementById("btn-3").addEventListener("click" , function(){
+    alert("bord update sucessfully")
+    const count = document.getElementById("count-num").innerText
+    const sub = count - 1
+    document.getElementById("count-num").innerText = sub
+   
+   
+    const add = document.getElementById("count-add").innerText
+    const convertNum = parseInt(add)
+    const sum = convertNum +1
+    document.getElementById("count-add").innerText = sum
+
+    const container = document.getElementById("trans-data")
+ 
+ const paid = document.createElement("p")
+ paid.innerText=` You have Complete The Optimize Home page at 12:48:15 PM
+ `
+ 
+container.appendChild(paid)
+
+    document.getElementById("btn-3").disabled = true
+  })
+
+
+// btton----4
+
+ document.getElementById("btn-4").addEventListener("click" , function(){
+    alert("bord update sucessfully")
+    
+    const count = document.getElementById("count-num").innerText
+    const sub = count - 1
+    document.getElementById("count-num").innerText = sub
+   
+   
+    const add = document.getElementById("count-add").innerText
+    const convertNum = parseInt(add)
+    const sum = convertNum +1
+    document.getElementById("count-add").innerText = sum
+    const container = document.getElementById("trans-data")
+ 
+ const paid = document.createElement("p")
+ paid.innerText=` You have Complete The Add new emoji 🤲at 12:48:15 PM
+ `
+ 
+container.appendChild(paid)
+
+    document.getElementById("btn-4").disabled = true
+  })
+
+//   btn----5
+
+ document.getElementById("btn-5").addEventListener("click" , function(){
+    alert("bord update sucessfully")
+    const count = document.getElementById("count-num").innerText
+    const sub = count - 1
+    document.getElementById("count-num").innerText = sub
+   
+   
+    const add = document.getElementById("count-add").innerText
+    const convertNum = parseInt(add)
+    const sum = convertNum +1
+    document.getElementById("count-add").innerText = sum
+    const container = document.getElementById("trans-data")
+ 
+ const paid = document.createElement("p")
+ paid.innerText=` You have Complete Integrate OpenAI APIat 12:48:15 PM
+ `
+ 
+container.appendChild(paid)
+
+    document.getElementById("btn-5").disabled = true
+  })
+     
+//   btn----6
+
+ document.getElementById("btn-6").addEventListener("click" , function(){
+    alert("bord update sucessfully")
+    alert("sucessfully")
+    const count = document.getElementById("count-num").innerText
+    const sub = count - 1
+    document.getElementById("count-num").innerText = sub
+   
+   
+    const add = document.getElementById("count-add").innerText
+    const convertNum = parseInt(add)
+    const sum = convertNum +1
+    document.getElementById("count-add").innerText = sum
+    const container = document.getElementById("trans-data")
+ 
+ const paid = document.createElement("p")
+ paid.innerText=` You have Complete The Improve Job searching at 12:48:15 PM
+ `
+ 
+container.appendChild(paid)
+
+    document.getElementById("btn-6").disabled = true
+  })
+
+  
+  document.getElementById("theme").addEventListener("click",function(){
+     
+    
+  })
+
+
+  
+  
---
+++ script/secand.js
@@ -0,0 +1,7 @@
+ document.getElementById("discover").addEventListener("click",function(){
+
+    window.location.href = "./find.html"
+ })
+ 
+
+ 
